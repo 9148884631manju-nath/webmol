69f6a7ac1ea145637c67ed6d81d31b1506531a65
Manjunath K <9148884631manju@gmail.com>
2026-04-29 12:08:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php require_once "lib/webmodule.php"; $wm=new WebMol("","lib/dbcon.json","newdb");
 
-//define("APP","mediapp");
-define("SERVER","http://localhost/WEBMOLv01/");
-
 if(isset($_REQUEST['page'])){$page=$_REQUEST['page'];}else{$page="";}
 
 switch($page){
-
-case "getstart": $fle= "tutorial/Welcome.php"; break;
-default: $fle= "home/Welcome.php"; break;
+default: $fle= "web/Welcome.html"; break;
 }
 
 $wm->page($fle);
